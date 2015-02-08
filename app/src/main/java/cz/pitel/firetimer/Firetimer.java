98f468d1071f077276039d438593be2ac6afc074
Ing. Jan Kaláb <pitlicek@gmail.com>
2015-02-08 17:19:30 +0100

    WIP

--- app/src/main/java/cz/pitel/firetimer/Firetimer.java
+++ app/src/main/java/cz/pitel/firetimer/Firetimer.java
@@ -4,17 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import butterknife.InjectView;
 
 
 public class Firetimer extends Activity {
+    @InjectView(R.id.button) Button button;
+    @InjectView(R.id.progress) ProgressBar progress;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
     }
 
-
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -36,4 +42,5 @@ public class Firetimer extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
